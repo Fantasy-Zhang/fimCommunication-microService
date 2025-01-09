dcbe0052817a0730eb66c7a6b550321fcd727161
zhanghao <2067162807@qq.com>
2025-01-09 23:23:56 +0800

    认证服务——认证接口

--- fim_auth/auth_api/internal/logic/authenticationlogic.go
+++ fim_auth/auth_api/internal/logic/authenticationlogic.go
@@ -2,6 +2,10 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"fimCommunication-microService/utils/jwts"
+	"fmt"
+	"time"
 
 	"fimCommunication-microService/fim_auth/auth_api/internal/svc"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -21,8 +25,22 @@ func NewAuthenticationLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Au
 	}
 }
 
-func (l *AuthenticationLogic) Authentication() (resp string, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+func (l *AuthenticationLogic) Authentication(token string) (resp string, err error) {
+	if token == "" {
+		err = errors.New("认证失败")
+		return
+	}
+	payload, err := jwts.ParseToken(token, l.svcCtx.Config.Auth.AccessSecret)
+	if err != nil {
+		err = errors.New("认证失败")
+		return
+	}
+	l.svcCtx.Redis.SetNX(l.ctx, fmt.Sprintf("logout_%d", payload.UserId), "", time.Duration(payload.ExpiresAt.Unix())*time.Second)
+	_, err = l.svcCtx.Redis.Get(l.ctx, fmt.Sprintf("logout_%d", payload.UserId)).Result()
+	if err != nil {
+		err = errors.New("认证失败")
+		return
+	}
+	resp = "认证通过"
+	return resp, nil
 }
